59628520bfd401b86533879d74b8193ca03d7203
Kaoru Kohashigawa <dev@kaoruk.com>
2016-07-03 08:50:01 -0700

    scaffold post

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160703154714) do
+ActiveRecord::Schema.define(version: 20160703154846) do
 
   create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
